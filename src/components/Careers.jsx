49c37019d4ae61982e7eb60081e92f08e582bce7
Dumebi Nwanze <davisnwanze7@gmail.com>
2023-09-27 13:30:50 +0300

    text center

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -4,7 +4,7 @@ function Careers() {
   return (
     <div id="careers"className=' flex flex-col items-center space-y-4 px-4 py-20 md:p-20 bg-background'>
 
-        <h2 className='font-extrabold text-4xl'>Join our <span className='text-secondary'>world class</span> team</h2>
+        <h2 className='font-extrabold text-4xl text-center'>Join our <span className='text-secondary'>world class</span> team</h2>
         <p className='text-md px-10 text-center'>
         Interested in joining our team and shaping the future of Career Launch Consultancy? We're always on the lookout for talented individuals who share our passion for career development and empowerment. Send us an email at <span className='text-secondary font-bold'>careers@clc.com</span> to inquire about current job openings or to submit your application. We look forward to hearing from you and exploring the exciting career opportunities awaiting you at CLC.
         </p>
